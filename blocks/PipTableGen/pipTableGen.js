37db8f2b3e41a1cfe6436e242d8ac89291c0b696
Алексей Жуковский <alexey@poravinternet.ru>
2020-05-07 20:41:51 +0300

    Добавил в генератор подстановку размера ячейки, доработал очистку данных

--- blocks/PipTableGen/pipTableGen.js
+++ blocks/PipTableGen/pipTableGen.js
@@ -1,6 +1,6 @@
 //Генерация таблицы Полотно с ячейками
 
-function tableGenerator(row, column, bgColor, borderColor) {
+function tableGenerator(row, column, bgColor, borderColor, cellWidth, cellHeight) {
 
 	let quotes = ''
 
@@ -9,21 +9,21 @@ function tableGenerator(row, column, bgColor, borderColor) {
 	//части таблиц из которых она собирается
 	let tableStart = '', tableEnd = '', tableRowStart = '', tableRowEnd = '', tableDataStart = '', tableDataEnd = '';
 
-	//если нужна с ковычками таблица
+	//Выбираем по парраметру  какую таблицу построить
 	if (quotes == '-q') {
-		tableStart = '"<table cellpadding="+""10""+" border="+""1""+" bordercolor="+""#' + borderColor +'""+" rules="+""all">';
+		tableStart = '"<table cellpadding="+""10""+" border="+""1""+" bordercolor="+""#' + borderColor + '""+" rules="+""all">';
 		tableEnd = '</table>"';
 		tableRowStart = '<tr>';
 		tableRowEnd = '</tr>';
-		tableDataStart = '<td align="+""center""+" valign="+""center""+" bgcolor="+""#' + bgColor +'""+" height="+""150""+" width="+""80">';
+		tableDataStart = '<td align="+""center""+" valign="+""center""+" bgcolor="+""#' + bgColor + '""+" height="+""150""+" width="+""80">';
 		tableDataEnd = '</td>';
 		//без кавычек таблица
 	} else {
-		tableStart = '<table cellpadding="10" border="1" bordercolor="#' + borderColor +'" rules="all">';
+		tableStart = '<table cellpadding="10" border="1" bordercolor="#' + borderColor + '" rules="all">';
 		tableEnd = '</table>';
 		tableRowStart = '<tr>';
 		tableRowEnd = '</tr>';
-		tableDataStart = '<td align="center" valign="center" bgcolor="#' + bgColor +'" height="150" width="80">';
+		tableDataStart = '<td align="center" valign="center" bgcolor="#' + bgColor + '" height="' + cellHeight + '" width="' + cellWidth + '">';
 		tableDataEnd = '</td>';
 	}
 	//собираем таблицу по правилам выше
@@ -43,55 +43,59 @@ function tableGenerator(row, column, bgColor, borderColor) {
 }
 
 //по проверка на возможность построения таблицы по парраметрам холста и ячейки
-function checkingValues (w1, h1, w2, h2) {
+function checkingValues(w1, h1, w2, h2) {
 	//Счётчик верно введенных парраметров
-	let count=0;
+	let count = 0;
 
 	//Серия условий проверки введённых парраметров и вывода сообщений если парраметр не является положительным числом
-	if (isNaN(w1)==false && Math.sign(w1)==1) {
+	if (isNaN(w1) === false && Math.sign(w1) == 1) {
 		$('#errorCanW').empty();
 		count++
 	} else {
 		$('#errorCanW').empty().append('Введите положительное число.');
 	}
 
-	if (isNaN(h1)==false && Math.sign(h1)==1) {
+	if (isNaN(h1) === false && Math.sign(h1) == 1) {
 		$('#errorCanH').empty();
 		count++
 	} else {
 		$('#errorCanH').empty().append('Введите положительное число.');
 	}
 
-	if (isNaN(w2)==false && Math.sign(w2)==1) {
+	if (isNaN(w2) === false && Math.sign(w2) == 1) {
 		$('#errorCellW').empty();
 		count++
 	} else {
 		$('#errorCellW').empty().append('Введите положительное число.');
 	}
 
-	if (isNaN(h2)==false && Math.sign(h2)==1) {
+	if (isNaN(h2) === false && Math.sign(h2) == 1) {
 		$('#errorCellH').empty();
 		count++
 	} else {
 		$('#errorCellH').empty().append('Введите положительное число.');
 	}
-
 console.log(count)
-
 	//проверим если парраметры введены верные, высота и ширина полотна больше либо равны высоте  и ширине ячейки, то таблицу можно строить
-	if (+w2>=+w2 && +h1>=+h2 && count==4) {
+	if (+w1 >= +w2 && +h1 >= +h2 && count == 4) {
 		$('.settings__error').empty();
 		return true
 	} else {
-		$('.settings__error').empty().append('Холст меньше ячейки.');
-		//Очистим предыдущую таблицу
-		$('.pipTableGen__build').empty();
+		if (count == 4) {
+			$('.settings__error').empty().append('Холст меньше ячейки.');
+			//Очистим предыдущую таблицу
+			$('.pipTableGen__build-wrap').empty();
+		}
 	}
 }
 
 //функция построение нужной таблицы с проверками
-var tableCode;
+let tableCode;
+
 function tableResult() {
+	//запустим функцию очистки данных
+	dataСleaning();
+
 //получим значение форм для генерации таблицы
 	var canvasHeight, canvasWidth, cellHeight, cellWidth, bgColor, borderColor;
 	canvasWidth = $('#canvas-width').val();
@@ -99,35 +103,49 @@ function tableResult() {
 	cellWidth = $('#cell-width').val();
 	cellHeight = $('#cell-height').val();
 	bgColor = $('#cell-color').val();
-	borderColor= $('#border-color').val();
+	borderColor = $('#border-color').val();
 
 
 	//проверим корректность значений и если все верно запустим генерацию таблицы
 	if (checkingValues(canvasWidth, canvasHeight, cellWidth, cellHeight)) {
 		//рассчитываем таблицу
 		let row, col;
-		row = Math.floor(+canvasWidth/+cellWidth);
-		col = Math.floor(+canvasHeight/+cellHeight);
+		row = Math.floor(+canvasWidth / +cellWidth);
+		col = Math.floor(+canvasHeight / +cellHeight);
 
 		//добавим данные о таблице
 		$('.pipTableGen__col').empty().append(col);
 		$('.pipTableGen__row').empty().append(row);
-		$('.pipTableGen__amount').empty().append(row*col);
-
-		tableCode=tableGenerator(row, col, bgColor, borderColor);
-		$('.pipTableGen__build').empty().append(tableCode);
+		$('.pipTableGen__amount').empty().append(row * col);
+		//Сгенерируем таблицу по парраметрам
+		tableCode = tableGenerator(row, col, bgColor, borderColor, cellWidth, cellHeight);
+		$('.pipTableGen__build-wrap').empty().append(tableCode);
+		//Контейнеру в который закинем таблицу определим минимальную ширину равную ширине полотна что бы ячейки не сжимались
+		$('.pipTableGen__build-wrap').css( 'min-width', canvasWidth + 'px')
+		//Активируем кнопку Скопировать
+		$('.pipTableGen__copy-btn').removeAttr("disabled");
 	}
 
+}
+
+// функция в которой хранится очистка данных перед построением таблицы
+function dataСleaning() {
+	//очистим от инлайн стилией контейнер таблицы
+	$('.pipTableGen__build-wrap').removeAttr("style");
+	// деактивируем кнопку Скопировать
+	$('.pipTableGen__copy-btn').attr("disabled", true);
+	//очистим значение переменной
+	tableCode='';
 }
 
 //по триггеру генерируем таблицу
 $('.trigger').on('click', tableResult);
 
 //копируем в буфер обмена
-$('.read-btn').on('click', function () {
-	navigator.clipboard.writeText(tableCode).then(function() {
+$('.pipTableGen__copy-btn').on('click', function () {
+	navigator.clipboard.writeText(tableCode).then(function () {
 		console.log('Async: Copying to clipboard was successful!');
-	}, function(err) {
+	}, function (err) {
 		console.error('Async: Could not copy text: ', err);
 	});
 });
--- build/js/script.js
+++ build/js/script.js
@@ -1840,7 +1840,7 @@ $(document).ready(function () {
   }
 }); //Генерация таблицы Полотно с ячейками
 
-function tableGenerator(row, column, bgColor, borderColor) {
+function tableGenerator(row, column, bgColor, borderColor, cellWidth, cellHeight) {
   var quotes = '';
   var content = ''; //переменная в которую сгенерируется код таблицы
 
@@ -1852,7 +1852,7 @@ function tableGenerator(row, column, bgColor, borderColor) {
       tableRowStart = '',
       tableRowEnd = '',
       tableDataStart = '',
-      tableDataEnd = ''; //если нужна с ковычками таблица
+      tableDataEnd = ''; //Выбираем по парраметру  какую таблицу построить
 
   if (quotes == '-q') {
     tableStart = '"<table cellpadding="+""10""+" border="+""1""+" bordercolor="+""#' + borderColor + '""+" rules="+""all">';
@@ -1866,7 +1866,7 @@ function tableGenerator(row, column, bgColor, borderColor) {
     tableEnd = '</table>';
     tableRowStart = '<tr>';
     tableRowEnd = '</tr>';
-    tableDataStart = '<td align="center" valign="center" bgcolor="#' + bgColor + '" height="150" width="80">';
+    tableDataStart = '<td align="center" valign="center" bgcolor="#' + bgColor + '" height="' + cellHeight + '" width="' + cellWidth + '">';
     tableDataEnd = '</td>';
   } //собираем таблицу по правилам выше
 
@@ -1896,28 +1896,28 @@ function checkingValues(w1, h1, w2, h2) {
   //Счётчик верно введенных парраметров
   var count = 0; //Серия условий проверки введённых парраметров и вывода сообщений если парраметр не является положительным числом
 
-  if (isNaN(w1) == false && Math.sign(w1) == 1) {
+  if (isNaN(w1) === false && Math.sign(w1) == 1) {
     $('#errorCanW').empty();
     count++;
   } else {
     $('#errorCanW').empty().append('Введите положительное число.');
   }
 
-  if (isNaN(h1) == false && Math.sign(h1) == 1) {
+  if (isNaN(h1) === false && Math.sign(h1) == 1) {
     $('#errorCanH').empty();
     count++;
   } else {
     $('#errorCanH').empty().append('Введите положительное число.');
   }
 
-  if (isNaN(w2) == false && Math.sign(w2) == 1) {
+  if (isNaN(w2) === false && Math.sign(w2) == 1) {
     $('#errorCellW').empty();
     count++;
   } else {
     $('#errorCellW').empty().append('Введите положительное число.');
   }
 
-  if (isNaN(h2) == false && Math.sign(h2) == 1) {
+  if (isNaN(h2) === false && Math.sign(h2) == 1) {
     $('#errorCellH').empty();
     count++;
   } else {
@@ -1926,13 +1926,15 @@ function checkingValues(w1, h1, w2, h2) {
 
   console.log(count); //проверим если парраметры введены верные, высота и ширина полотна больше либо равны высоте  и ширине ячейки, то таблицу можно строить
 
-  if (+w2 >= +w2 && +h1 >= +h2 && count == 4) {
+  if (+w1 >= +w2 && +h1 >= +h2 && count == 4) {
     $('.settings__error').empty();
     return true;
   } else {
-    $('.settings__error').empty().append('Холст меньше ячейки.'); //Очистим предыдущую таблицу
+    if (count == 4) {
+      $('.settings__error').empty().append('Холст меньше ячейки.'); //Очистим предыдущую таблицу
 
-    $('.pipTableGen__build').empty();
+      $('.pipTableGen__build-wrap').empty();
+    }
   }
 } //функция построение нужной таблицы с проверками
 
@@ -1940,7 +1942,9 @@ function checkingValues(w1, h1, w2, h2) {
 var tableCode;
 
 function tableResult() {
-  //получим значение форм для генерации таблицы
+  //запустим функцию очистки данных
+  dataСleaning(); //получим значение форм для генерации таблицы
+
   var canvasHeight, canvasWidth, cellHeight, cellWidth, bgColor, borderColor;
   canvasWidth = $('#canvas-width').val();
   canvasHeight = $('#canvas-height').val();
@@ -1957,16 +1961,31 @@ function tableResult() {
 
     $('.pipTableGen__col').empty().append(col);
     $('.pipTableGen__row').empty().append(row);
-    $('.pipTableGen__amount').empty().append(row * col);
-    tableCode = tableGenerator(row, col, bgColor, borderColor);
-    $('.pipTableGen__build').empty().append(tableCode);
+    $('.pipTableGen__amount').empty().append(row * col); //Сгенерируем таблицу по парраметрам
+
+    tableCode = tableGenerator(row, col, bgColor, borderColor, cellWidth, cellHeight);
+    $('.pipTableGen__build-wrap').empty().append(tableCode); //Контейнеру в который закинем таблицу определим минимальную ширину равную ширине полотна что бы ячейки не сжимались
+
+    $('.pipTableGen__build-wrap').css('min-width', canvasWidth + 'px'); //Активируем кнопку Скопировать
+
+    $('.pipTableGen__copy-btn').removeAttr("disabled");
   }
+} // функция в которой хранится очистка данных перед построением таблицы
+
+
+function dataСleaning() {
+  //очистим от инлайн стилией контейнер таблицы
+  $('.pipTableGen__build-wrap').removeAttr("style"); // деактивируем кнопку Скопировать
+
+  $('.pipTableGen__copy-btn').attr("disabled", true); //очистим значение переменной
+
+  tableCode = '';
 } //по триггеру генерируем таблицу
 
 
 $('.trigger').on('click', tableResult); //копируем в буфер обмена
 
-$('.read-btn').on('click', function () {
+$('.pipTableGen__copy-btn').on('click', function () {
   navigator.clipboard.writeText(tableCode).then(function () {
     console.log('Async: Copying to clipboard was successful!');
   }, function (err) {
