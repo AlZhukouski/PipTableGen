e6147b6c068ed70053a8bd20b3c35b9b8cde0054
Алексей Жуковский <alexey@poravinternet.ru>
2020-05-15 12:44:32 +0300

    Пересобрал билд

--- build/js/script.js
+++ build/js/script.js
@@ -15,40 +15,10 @@ jQuery(document).ready(function () {
       });
     });
   }
-}); // Скрипт добавления файлов в форму
-
-(function ($, window, document, undefined) {
-  $('.form__fileinput').each(function () {
-    var $input = $(this),
-        $label = $input.next('label'),
-        labelVal = $label.html();
-    $input.on('change', function (e) {
-      var fileName = '';
-      $(this).siblings('.form__fileinput-close').addClass('form__fileinput-close--active');
-      if (this.files && this.files.length > 1) fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);else if (e.target.value.length > 30) {
-        fileName = e.target.value.split('\\').pop().slice(0, 30) + '...';
-      } else {
-        fileName = e.target.value.split('\\').pop();
-      }
-      if (fileName) $label.find('span').html(fileName);else $label.html(labelVal);
-    }); // Firefox bug fix
-
-    $input.on('focus', function () {
-      $input.addClass('js_has-focus');
-    }).on('blur', function () {
-      $input.removeClass('js_has-focus');
-    });
-  });
-  $('.form__fileinput-close').click(function () {
-    $(this).siblings('.form__fileinput').val('');
-    $(this).prev().find('span').html('Прикрепить файл');
-    $(this).removeClass('form__fileinput-close--active');
-  });
-})(jQuery, window, document);
+});
 /*! Magnific Popup - v1.1.0 - 2016-02-20
  * http://dimsemenov.com/plugins/magnific-popup/
  * Copyright (c) 2016 Dmitry Semenov; */
-
 
 ;
 
@@ -1838,7 +1808,37 @@ $(document).ready(function () {
   function closePopup() {
     $.magnificPopup.close();
   }
-}); //Генерация таблицы Полотно с ячейками
+}); // Скрипт добавления файлов в форму
+
+(function ($, window, document, undefined) {
+  $('.form__fileinput').each(function () {
+    var $input = $(this),
+        $label = $input.next('label'),
+        labelVal = $label.html();
+    $input.on('change', function (e) {
+      var fileName = '';
+      $(this).siblings('.form__fileinput-close').addClass('form__fileinput-close--active');
+      if (this.files && this.files.length > 1) fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);else if (e.target.value.length > 30) {
+        fileName = e.target.value.split('\\').pop().slice(0, 30) + '...';
+      } else {
+        fileName = e.target.value.split('\\').pop();
+      }
+      if (fileName) $label.find('span').html(fileName);else $label.html(labelVal);
+    }); // Firefox bug fix
+
+    $input.on('focus', function () {
+      $input.addClass('js_has-focus');
+    }).on('blur', function () {
+      $input.removeClass('js_has-focus');
+    });
+  });
+  $('.form__fileinput-close').click(function () {
+    $(this).siblings('.form__fileinput').val('');
+    $(this).prev().find('span').html('Прикрепить файл');
+    $(this).removeClass('form__fileinput-close--active');
+  });
+})(jQuery, window, document); //Генерация таблицы Полотно с ячейками
+
 
 function tableGenerator(row, column, bgColor, borderColor, cellsize1, cellsize2, cellContentsOption, quotes) {
   var content = ''; //переменная в которую сгенерируется код таблицы
